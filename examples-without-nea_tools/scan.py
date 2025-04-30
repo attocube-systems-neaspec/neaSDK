c60a4fdd1418ee5cdcefab8a2c28d060dd8dd8d2
neaspec SDK <118909478+neaspec-SDK@users.noreply.github.com>
2025-04-30 14:02:50 +0200

    improved comments

--- examples-without-nea_tools/scan.py
+++ examples-without-nea_tools/scan.py
@@ -4,7 +4,7 @@ import Nea.Client.SharedDefinitions as nea
 # create copy of most recent scan which is currently displayed
 recent_params = context.Microscope.Py.Preview.ScanParamters.Spawn()
 
-# create new ScanParameters for AFM
+# create new ScanParameters for PsHet Imaging
 new_params = context.Microscope.Py.CreateRoute2DScanParameters(
     areaWidth=5, # in µm
     areHeight=5, # in µm
@@ -12,8 +12,8 @@ new_params = context.Microscope.Py.CreateRoute2DScanParameters(
     rows=100, # in px
 )
 
-# change to ContactMode and change other parameters
-new_params.ScanMode = nea.ScanMode.ContactMode2D
+# change scan to standard AFM mode and change other parameters
+new_params.ScanMode = nea.ScanMode.Afm
 new_params.TargetMillisecondsPerPixel = 10.2 # in ms/px
 
 # run scan and write results to database
