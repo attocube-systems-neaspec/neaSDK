72face5f3728538f9314aec60198e3d06237fe00
neaspec SDK <118909478+neaspec-SDK@users.noreply.github.com>
2025-04-30 14:35:41 +0200

    Create approach.py

---
+++ examples-without-nea_tools/approach.py
@@ -0,0 +1,25 @@
+import sys
+from neaspec import context
+
+neascan_console = sys.version_info.minor == 4
+
+def approach(setpoint=0.8):
+    # exit if system is already in contact
+    if context.Microscope.Py.IsRegulatorSetpointReachedZ:
+        return 
+    
+    # exit if system is already approaching
+    if context.Logic.IsApproachingSample:
+        return 
+    
+    # check if approach is possible
+    if context.Logic.ApproachSample.CanExecute(setpoint):
+        context.Logic.ApproachSample(setpoint)
+    else:
+        state = context.Logic.CurrentState if neascan_console else nea.IPyLogic(context.Logic).CurrentState
+        print("Cannot approach in current state", state)
+        return
+
+    # sleep until system has reached contact   
+    while not context.Microscope.Py.IsRegulatorSetpointReachedZ:
+        sleep(0.1, False)
