aa29e5df5bb0f08e390340a1cebd3270f0891e25
neaspec SDK <118909478+neaspec-SDK@users.noreply.github.com>
2025-10-06 16:38:18 +0200

    add getting scan data

--- examples-without-nea_tools/search_database.py
+++ examples-without-nea_tools/search_database.py
@@ -5,17 +5,36 @@ Access neaSCOPE database for searching specified keyword.
   a. all Locations with keyword and 
   b. all Scans with keyword
    which are included in the project
+3. Return the M1A channel for each found scan.
 """
 from neaspec import context
+import System.Threading as Threading
+import Nea.Client.SharedDefinitions as nea
+import System
 keyword = "FAT"
 maxcount = 100
+channel_name = "M1A"
 projects = context.Database.SelectProjects(fullTextSearch=keyword, maxProjectsCount = maxcount).Result
- 
+print(f"Num projects with keyword '{keyword}' = {len(projects)}")
+
+def get_scan_data(scan_id, channel_name):
+  progress = System.Progress[float]()
+  NoCancellation = getattr(Threading.CancellationToken, 'None')
+  channel = nea.Channel.FromString(channel_name)
+  data = context.Database.SelectRawImage(scan_id,channel, 1, NoCancellation, progress).Result
+  return data
+
 for project in projects:
-    locations = context.Database.SelectLocations(project.Id).Result
+    locations = context.Database.SelectLocations(projectId = project.Id).Result
+    print(f"Project '{project.Name}': {len(locations)} location(s)")
+
     for location in locations:
-        print(f"Location {location.Id} has keyword: {keyword in locations.Name}")
-        location_scans = context.Database.SelectScans(location=location)
+        location_scans = context.Database.SelectScans(location=location, projectId = project.Id).Result
+        print(f"Location '{location.Name}' of project '{project.Name}' has {len(location_scans)} scan(s)")
+    
     scans = context.Database.SelectScans(projectId = project.Id, fullTextSearch = keyword, maxScansCount = maxcount, skipScansCount=0).Result
+    print(f"Project {project.Name}: {len(scans)} scan(s) with keyword '{keyword}'")
+
     for scan in scans:
-        print(scan.Name)
+        data = get_scan_data(scan.Uuid, channel_name)
+        print(f"Scan '{scan.Name}' has length {len(data)}, first value is {data[0]}")
